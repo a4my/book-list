bcb358598e837b98f46054a97f5e4e8889d345fc
Alex Fourmy <alexfourmy@gmail.com>
2021-04-19 14:01:27 +0200

    Commpleted local storage

--- appes6.js
+++ appes6.js
@@ -53,6 +53,53 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
+    }
+
+    return books
+  }
+  static displayBooks() {
+    const books = Store.getBooks()
+
+    books.forEach(function(book) {
+      const ui = new UI
+
+      // Add book to UI
+      ui.addBookToList(book)
+    })
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks()
+
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks()
+
+    books.forEach(function(book, index) {
+      if(book.isbn === isbn) {
+        books.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+}
+
+//DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
+
 // Event listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
@@ -74,6 +121,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     // Add book to list
     ui.addBookToList(book)
 
+    // Add to LS
+    Store.addBook(book)
+
     // Show success
     ui.showAlert('Book added!', 'success')
 
@@ -92,6 +142,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
   // delete book
   ui.deleteBook(e.target)
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
   // SHow message
   ui.showAlert('Book removed!', 'success')
