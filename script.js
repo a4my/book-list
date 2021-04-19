229bce7ab5ed24504b6e07d7154dda0152810f25
Alex Fourmy <alexfourmy@gmail.com>
2021-04-19 13:11:16 +0200

    Commpleted project

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000)
 }
 
+// Delete book
+UI.prototype.deleteBook = function(target){
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove()
+  }
+}
+
 // Clear fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -53,7 +60,7 @@ UI.prototype.clearFields = function() {
   document.getElementById('isbn').value = '';
 }
 
-// Event listeners
+// Event listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -83,4 +90,18 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   }
      
   e.preventDefault()
+})
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e){
+  // Instantiate UI
+  const ui = new UI()
+
+  // delete book
+  ui.deleteBook(e.target)
+
+  // SHow message
+  ui.showAlert('Book removed!', 'success')
+
+  e.preventDefault
 })
